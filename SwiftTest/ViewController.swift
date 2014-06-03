bf63f066a6ce51d1465d44ead2c27788dd718e4c
Josh Lapham <josh@joshlapham.com>
2014-06-03 13:01:14 +1000

    Added UIAlertView to list view.

--- SwiftTest/ViewController.swift
+++ SwiftTest/ViewController.swift
@@ -8,18 +8,15 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class ViewController: UITableViewController, UIAlertViewDelegate {
                             
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "List"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
     
+    // UITableView delegate methods
     override func numberOfSectionsInTableView(tableView: UITableView!) -> Int {
         return 1
     }
@@ -31,9 +28,32 @@ class ViewController: UITableViewController {
     override func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
         let cell = tableView.dequeueReusableCellWithIdentifier("TableCell", forIndexPath: indexPath) as UITableViewCell
         
-        cell.textLabel.text = "Current row is \(indexPath.row)"
+        cell.textLabel.text = "This is row \(indexPath.row)"
         
         return cell
+    }
+    
+    override func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
+        // Pass row number to showMyAlert function and show alert
+        self.showMyAlert(indexPath.row)
+    }
+    
+    // UIAlertView delegate methods
+    func alertView(alertView: UIAlertView!, didDismissWithButtonIndex buttonIndex: Int) {
+        NSLog("Did dismiss button: %d", buttonIndex)
+    }
+    
+    // Function to init a UIAlertView and show it
+    func showMyAlert(rowToUseInAlert: NSInteger!) {
+        var myAlert = UIAlertView()
+        
+        myAlert.delegate = self
+        myAlert.title = "Alert!"
+        myAlert.message = "You selected row \(rowToUseInAlert)"
+        myAlert.addButtonWithTitle("Cancel")
+        myAlert.addButtonWithTitle("OK")
+        
+        myAlert.show()
     }
 }
 
